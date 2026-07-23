0d30e3c4d5ab9ec2d473810a039d88691117f6f6
pierre <pierrerussellhojun@gmail.com>
2026-07-23 11:04:00 +0800

    test

--- src/BizModSwapBot.API/Program.cs
+++ src/BizModSwapBot.API/Program.cs
@@ -35,7 +35,7 @@ if (app.Environment.IsDevelopment())
 
 if (app.Environment.IsProduction())
 {
-    app.UseMiddleware<TelegramAuthMiddleware>();
+    // app.UseMiddleware<TelegramAuthMiddleware>();
 }
 
 app.MapControllers();
